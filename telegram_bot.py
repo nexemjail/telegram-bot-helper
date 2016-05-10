020f52838e8f23738957fc2b8049a4ef2bddc6c2
Alex <nexemjail@gmail.com>
2016-05-10 15:39:35 +0300

    some refactor?

--- telegram_bot.py
+++ telegram_bot.py
@@ -44,61 +44,64 @@ def _start(bot, update):
 
     bot.watson_info['conversation_id'] = response['conversation_id']
     bot.watson_info['client_id'] = response['client_id']
-
-    bot.sendMessage(chat_id=update.message.chat_id,
-                    text='\n'.join(response['response']))
+    for message in response:
+        bot.sendMessage(chat_id=update.message.chat_id,
+                    text=message)
     print('Message sent!')
 
 
 def _echo(bot, update):
     print('Got a message!')
-    if update.message.text or True:
-        print("I't is a text one!")
-        response = bot.dialog.conversation(
-            bot.watson_info['dialog_id'],
-            update.message.text, bot.watson_info['client_id'],
-            bot.watson_info['conversation_id'])
-        print(response)
+    print("I't is a text one!")
+    response = bot.dialog.conversation(
+        bot.watson_info['dialog_id'],
+        update.message.text, bot.watson_info['client_id'],
+        bot.watson_info['conversation_id'])
+    print(response)
+    text = response['response']
+    print('Got a response from Dialog')
+
+    print('Checking for profile variables')
+    variables = bot.dialog.get_profile(
+        bot.watson_info['dialog_id'], bot.watson_info['client_id'])
+    print('Profile variables ' + str(variables))
+
+    got_twitter = variables and variables['name_values'] \
+            and variables['name_values'][0] \
+            and variables['name_values'][0]['name'] \
+            and variables['name_values'][0]['name'] == 'TwitterAccount' \
+            and variables['name_values'][0]['value'] \
+            and variables['name_values'][0]['value'].startswith('@')
+
+    if got_twitter:
+        print('It\'s twitter!')
+        obama_text = ''
+        with open('texts/obama_text.txt', 'r') as f:
+            obama_text = f.read()
+        response = bot.personality_insights.profile(obama_text)
+        print('Got a response from PI')
+        print('Updating profile variables')
+        response_profile = bot.dialog.update_profile(bot.watson_info['dialog_id'],
+                                                     {'UniversityName': 'Princeton',
+                                                      'UniversityWebsite': 'http://www.princeton.edu/main/'},
+                                                     bot.watson_info['client_id'])
+
+        print('Sending continue constant to conversation')
+        response = bot.dialog.conversation(bot.watson_info['dialog_id'],
+                                           CONTINUE_CONSTANT, bot.watson_info['client_id'],
+                                           bot.watson_info['conversation_id'])
         text = response['response']
-        print('Got a response from Dialog')
 
-        print('Checking for profile variables')
-        variables = bot.dialog.get_profile(
-            bot.watson_info['dialog_id'], bot.watson_info['client_id'])
-        print('Profile variables ' + str(variables))
+    print('Sening a message')
+    print(str(text))
+    for message in text:
+        bot.sendMessage(update.message.chat_id, text=str(message))
 
-        got_twitter = variables and variables['name_values'] \
-                and variables['name_values'][0] \
-                and variables['name_values'][0]['name'] \
-                and variables['name_values'][0]['name'] == 'TwitterAccount' \
-                and variables['name_values'][0]['value'] \
-                and variables['name_values'][0]['value'].startswith('@')
-
-        if got_twitter:
-            print('It\'s twitter!')
-            obama_text = ''
-            with open('texts/obama_text.txt', 'r') as f:
-                obama_text = f.read()
-            response = bot.personality_insights.profile(obama_text)
-            print('Got a response from PI')
-            print('Updating profile variables')
-            response_profile = bot.dialog.update_profile(bot.watson_info['dialog_id'],
-                                                         {'UniversityName': 'Princeton',
-                                                          'UniversityWebsite': 'http://www.princeton.edu/main/'},
-                                                         bot.watson_info['client_id'])
-
-            print('Sending continue constant to conversation')
-            response = bot.dialog.conversation(bot.watson_info['dialog_id'],
-                                               CONTINUE_CONSTANT, bot.watson_info['client_id'],
-                                               bot.watson_info['conversation_id'])
-            text = response['response']
-
-        print('Sening a message')
-        print(str(text))
-        bot.sendMessage(update.message.chat_id, text='\n'.join(text))
+# else:
+#     bot.sendMessage(update.message.chat_id, text="Don't know! Rly, help me and try again!")
+    print('Message sent!')
     # else:
-    #     bot.sendMessage(update.message.chat_id, text="Don't know! Rly, help me and try again!")
-        print('Message sent!')
+    #     bot.sendMessage(update.message.chat_id, text="Don't know you, m8!")
 
 
 def init_bot():
@@ -106,10 +109,12 @@ def init_bot():
     # logger.setLevel(logging.INFO)
     bot = telegram.Bot(token=bot_token)
     updater = Updater(token=bot_token)
-    start_handler = CommandHandler('start', _start,)
+    updater.dispatcher.handlers = []
+    start_handler = CommandHandler('start', _start)
     updater.dispatcher.addHandler(start_handler)
-    message_handler = MessageHandler([Filters.text, Filters.command], _echo)
+    message_handler = MessageHandler([Filters.text], _echo)
     updater.dispatcher.addHandler(message_handler)
+
     print(updater.dispatcher.handlers)
     return updater, bot
 
@@ -123,6 +128,7 @@ def stop_bot(updater):
 
 
 def clear_dispatchers(updater):
+    updater.dispatcher.re
     pass
 
 
